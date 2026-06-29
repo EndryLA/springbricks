32df8a0187a9080d7c47a511f48c4231a7e125a5
Endry Lundy <clansdoli@gmail.com>
2026-06-29 21:20:58 +0200

    initial commit

---
+++ springbricks/src/main/java/com/endryla/springbricks/SpringbricksApplication.java
@@ -0,0 +1,13 @@
+package com.endryla.springbricks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbricksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbricksApplication.class, args);
+	}
+
+}
---
+++ springbricks/src/test/java/com/endryla/springbricks/SpringbricksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endryla.springbricks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbricksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
